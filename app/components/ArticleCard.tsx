12fea2e7a82232cfb6a75a2c6a17a24acf144187
Richi <richipineda2@gmail.com>
2023-09-05 11:45:28 +0100

    added props, removed <a> from Link

--- app/components/ArticleCard.tsx
+++ app/components/ArticleCard.tsx
@@ -1,37 +1,34 @@
 import Link from "next/link"
 
 interface IProps {
+  title: string,
+  description: string,
+  portfolio?: boolean,
+  coverImage?: string,
 
 }
 
-const ArticleCard = ({
-  portfolio = false,
-  coverImage = '',
-}) => {
+const ArticleCard = ({ title = '', description = '', portfolio = false, coverImage = '', }: IProps) => {
 
   return (
     <div className="mb-14 flex flex-col sm:flex-row w-full">
-      <Link
-        href={""}
+      <Link href={""} className={`w-full text-gray-500 dark:text-gray-300
+        ${coverImage ? 'sm:w-1/2 lg:w-1/2 xl:w-3/5' : 'sm:w-4/5'}`}
       >
-        <a
-          className={`w-full text-gray-500 dark:text-gray-300 ${coverImage ? 'sm:w-1/2 lg:w-1/2 xl:w-3/5' : 'sm:w-4/5'
-            }`}
-        >
-          <h3 className="text-2xl text-gray-600 dark:text-gray-100">title</h3>
-          {!portfolio && (
-            <p className="text-sm my-1">
-              {/* <span>{moment(date).format('Do MMMM YYYY')}</span> */}<span>date</span>
-              <span className="px-1">-</span>
-              {/* <span>{tags.join(', ')}</span> */}
-              <span>tags</span>
-            </p>
-          )}
-          <p className="text-base mt-2">description</p>
-          <p className="text-base mt-2 underline hover:text-gray-800 dark:hover:text-gray-100 transition-colors">
-            Read more
+        <h3 className="text-2xl text-gray-600 dark:text-gray-100">title</h3>
+        {!portfolio && (
+          <p className="text-sm my-1">
+            {/* <span>{moment(date).format('Do MMMM YYYY')}</span> */}<span>date</span>
+            <span className="px-1">-</span>
+            {/* <span>{tags.join(', ')}</span> */}
+            <span>tags</span>
           </p>
-        </a>
+        )}
+
+        <p className="text-base mt-2">description</p>
+        <p className="text-base mt-2 underline hover:text-gray-800 dark:hover:text-gray-100 transition-colors">
+          Read more
+        </p>
       </Link>
       {coverImage && (
         <div className="w-full sm:w-1/2 lg:w-1/2 xl:w-2/5 mt-4 sm:mt-0 sm:ml-4 flex items-center justify-center">
@@ -43,3 +40,4 @@ const ArticleCard = ({
 }
 
 export default ArticleCard
+
