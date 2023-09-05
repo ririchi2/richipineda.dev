a820b41fd846701965b5fe219b607000f5d9ae11
Richi <richipineda2@gmail.com>
2023-09-05 14:11:21 +0100

    changed responsiveness in small screen, having the section be much wider

--- app/components/Section.tsx
+++ app/components/Section.tsx
@@ -9,9 +9,10 @@ const linebreakStyles = 'mt-24 md:mt-32 xl:mt-44'
 
 const Section = ({ children, linebreak = false }: IProps) => (
     <section
-        className={`text-base sm:text-lg font-light leading-relaxed lg:w-4/5 2xl:w-2/3 text-gray-600 dark:text-gray-300 ${
-            linebreak ? linebreakStyles : 'mt-12'
-        }`}
+        className={`text-base sm:text-lg font-light leading-relaxed
+        w-4/5 lg:w-4/5 2xl:w-2/3 text-gray-600 dark:text-gray-300
+        ${linebreak ? linebreakStyles : 'mt-12'}`
+        }
     >
         {children}
     </section>
