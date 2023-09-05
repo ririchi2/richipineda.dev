58d47178d00b86caab73f8e70a6e0b1845280ca5
Richi <richipineda2@gmail.com>
2023-09-05 14:44:52 +0100

    creating projects data

---
+++ app/data/projects.ts
@@ -0,0 +1,29 @@
+export const projects:
+  {
+    title: string,
+    description: string,
+    url: string,
+    coverImage?: string,
+    icon?: boolean,
+    portfolio?: boolean,
+  }[] =
+  [
+    {
+      title: 'Patientor Backend',
+      description: 'Patientor is a TypeScript and Express-based medical record application designed to manage diagnoses and basic health information of patients.',
+      coverImage: '',
+      url: 'https://github.com/ririchi2/patientor-backend',
+    },
+    {
+      title: 'Patientor Frontend',
+      description: 'Patientor Frontend complements the Patientor ecosystem by providing a user-friendly TypeScript-based interface.',
+      coverImage: '',
+      url: 'https://github.com/ririchi2/patientorx  ',
+    },
+    {
+      title: 'doIt - A To-Do List App',
+      description: 'doIt is a client-side, user-friendly To-Do List application designed to help you stay organized and on top of your tasks.',
+      coverImage: '',
+      url: 'https://github.com/ririchi2/todo-app',
+    },
+  ];
--- lib/utils.ts
+++
@@ -1,6 +0,0 @@
-export const isActiveLink = (href: string, currentPathname: string): boolean => {
-  if (currentPathname === '/') {
-    return href === currentPathname;
-  }
-  return currentPathname.startsWith(href);
-}
