cba1fba672b548b39996f205f22365761983daa3
Richi <richipineda2@gmail.com>
2023-09-06 20:57:44 +0100

    we dont give up in google analytics

--- app/components/GoogleAnalytics.tsx
+++ app/components/GoogleAnalytics.tsx
@@ -31,17 +31,29 @@
 //GoogleAnalytics.tsx
 
 "use client";
-import { log } from "console";
 import Script from "next/script";
+import { usePathname, useSearchParams } from 'next/navigation'
+import { useEffect } from "react";
+import { pageview } from "../lib/gtagHelper";
+
+
 
 const GoogleAnalytics = () => {
-
+  const pathname = usePathname()
+  const searchParams = useSearchParams()
   const GA_MEASUREMENT_ID = process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID
 
   if (!GA_MEASUREMENT_ID) {
     console.log("No GA_MEASUREMENT_ID");
     return null
   }
+
+  useEffect(() => {
+    const url = pathname + searchParams.toString()
+
+    pageview(GA_MEASUREMENT_ID, url);
+
+  }, [pathname, searchParams, GA_MEASUREMENT_ID]);
 
   return (
     <>
