50d2fdea4a329ce74074539a260bf8790935ac66
Richi <richipineda2@gmail.com>
2023-09-06 12:47:47 +0100

    fix on googleanalytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -41,9 +41,7 @@ export default function RootLayout({
               window.dataLayer = window.dataLayer || [];
               function gtag(){dataLayer.push(arguments);}
               gtag('js', new Date());
-              gtag('config', '${GA_MEASUREMENT_ID}', {
-              page_path: window.location.pathname,
-              });
+              gtag('config', '${GA_MEASUREMENT_ID}');
             `,
           }}
         />
